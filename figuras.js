eca431355f5d6d3040b98adcbd1402b3e774425f
joaquinvazquez <jromanvz@gmail.com>
2022-07-18 16:21:19 -0500

    Interactuando con HTMl

--- figuras.js
+++ figuras.js
@@ -1,3 +1,4 @@
+//Acá interactuamos con el HTML
 //Código del cuadrado
 console.group("Cuadrados");
 
@@ -13,19 +14,61 @@ areaCuadrado();
 
 console.groupEnd();
 
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+function calcularAreaCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
 //Código del triángulo
 console.group("Triangulo");
 
 function perimetroTriagulo(lado1, lado2, base){
-    return lado1 + lado2 + base;
+    return Number(lado1) + Number(lado2) + Number(base);
 }
 perimetroTriagulo();
 
+//Triangulo
+//perimetro triangulo
+function calcularPerimetroTriangulo(){
+    const inputLadoUno = document.getElementById("inputLadoUnoTriangulo");
+    const valueLadoUno = inputLadoUno.value;
+    
+    const inputLadoDos = document.getElementById("inputLadoDosTriangulo");
+    const valueLadoDos = inputLadoDos.value;
+    
+    const inputBase = document.getElementById("inputBaseTriangulo");
+    const valueBase = inputBase.value;
+
+    const perimetro = perimetroTriagulo(valueLadoUno, valueLadoDos, valueBase);
+    alert(perimetro);
+}
+//Area triangulo
 function areaTriangulo(base, altura){
     return base * altura / 2;
 }
 areaTriangulo();
 console.groupEnd();
+function calcularAreaTriangulo(){
+   
+    const base = Number(document.getElementById("inputBaseTrianguloA").value);
+    // const valueBase = inputBase.value;
+
+    const altura = Number(document.getElementById("inputAlturaTriangulo").value);
+    // const valueAltura = inputAltura.value;
+
+    const area = areaTriangulo(base, altura);
+    alert(area);
+}
+
 
 //Código del circulo
 console.group("Circulo");
@@ -33,6 +76,13 @@ console.group("Circulo");
 function diametroCirculo(radio){
     return radio * 2;
 };
+
+function calcularDiametroCirculo(){
+    const radio = Number(document.getElementById("inputRadioCirculo").value);
+
+    const diametro = diametroCirculo(radio);
+    alert(diametro);
+}
 diametroCirculo();
 //PI
 const pi = Math.PI; 
@@ -42,9 +92,23 @@ function perimetroCirculo(radio){
     return diametro * pi; 
 };
 perimetroCirculo();
+
+function calcularCircunferenciaCirculo(){
+    const inputRadioCircunferencia = Number(document.getElementById("inputRadioCirculo").value);
+
+    const circunferencia = perimetroCirculo(inputRadioCircunferencia);
+    alert(circunferencia);
+}
 //Área
 function areaCirculo (radio){
     return (radio * radio) * pi;
 };
 areaCirculo();
+
+function calcularAreaCirculo(){
+    const inputRadioCirculo = Number(document.getElementById("inputRadioCirculo").value);
+
+    const area = areaCirculo(inputRadioCirculo);
+    alert(area);
+}
 console.groupEnd("");
